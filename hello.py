2404ffe36ab0cea43327ad73c39a783c57b0c424
marvinkunst <marvin.kunst@gmx.de>
2020-07-06 12:15:18 +0200

    Überarbeitet

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 toppings = []
+print("Geben Sie ihre Toppings für die Pizza ein! ")
+print('press q to end')
+
 while True:
     topping = input("What should be on your Pizza? ")
     if topping == 'q':
@@ -6,6 +9,6 @@ while True:
     else:
         toppings.append(topping)
 
-
-print(toppings)
-print("Leckere Pizza")
+print("Ihre Pizza hat folgende Toppings:")
+for topping in toppings:
+    print(f'-{topping.title()}')
